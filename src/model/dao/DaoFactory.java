22c66e48f82fb852e10f88cc4cb26363678dbfdf
cccaiogv <itsscaiin@gmail.com>
2025-12-11 12:00:00 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,13 @@
 package model.dao;
 
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
 
+	public static SellerDao createSellerdao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	
 }
